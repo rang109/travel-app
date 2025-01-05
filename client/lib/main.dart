92e37cc46db8a946f5bc649a59ddac79a3cbb31d
rokkunbruv <themorghiknight579@gmail.com>
2025-01-05 10:48:52 +0800

    set up flutter codebase

---
+++ client/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:client/pages/home_page.dart';
+
+void main() {
+  runApp(const TravelApp());
+}
+
+// Root Widget
+class TravelApp extends StatelessWidget {
+  const TravelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Travel App',
+      theme: ThemeData(
+        // This is the theme of your application.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomePage(),
+    );
+  }
+}
