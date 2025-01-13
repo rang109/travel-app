4cb2d49d3886ab407eec208c3499091654b8954a
togi-sama <jeceneligot2004@gmail.com>
2025-01-13 18:02:44 +0800

    template update

--- travelAuth/travelApp/views.py
+++ travelAuth/travelApp/views.py
@@ -8,8 +8,25 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
 from django.core.mail import EmailMessage
+from django.contrib.auth import get_user_model
 
-def activate(request, uid64, token):
+def activate(request, uidb64, token):
+    User = get_user_model()
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+
+        messages.success(request, 'Thank you for your email confirmation. Now you can login your account.')
+        return redirect('login')
+    else:
+        messages.error(request, 'Activation link is invalid!')
+    
     return redirect('homepage')
 
 def activateEmail(request, user, to_email):
