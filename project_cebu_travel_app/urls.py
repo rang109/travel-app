a24f908022d28716b8d8d54f9ecbe6bd514d6c52
khing2001 <150302758+khing2001@users.noreply.github.com>
2025-01-15 21:29:30 +0800

    Add files via upload

---
+++ project_cebu_travel_app/urls.py
@@ -0,0 +1,27 @@
+"""
+URL configuration for project_cebu_travel_app project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to Cebu Travel App! gatal")
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home), # this is to define a simple home route
+    path('', include('tourism.urls')), # this includes urls from the tourism app
+]
---
+++ tourism/__init__.py
@@ -0,0 +1,1 @@
+#can remain as blank
