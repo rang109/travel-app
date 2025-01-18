a68e50571910c37231660d6e6e793acc171f7feb
rokkunbruv <themorghiknight579@gmail.com>
2025-01-18 22:04:44 +0800

    updated .gitignore

--- client/macos/Flutter/GeneratedPluginRegistrant.swift
+++ client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import is_valid
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  IsValidPlugin.register(with: registry.registrar(forPlugin: "IsValidPlugin"))
 }
