aa132642bbf0e707021dfad70532f404f1d9a797
conxant0 <confox2145@gmail.com>
2025-01-22 20:00:40 +0800

    added .env file to make sure that the password and email is secure, env file is in discord and you must  install python-decouple using pip

--- otp_validation/settings.py
+++ otp_validation/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+import os
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,7 +132,7 @@ AUTH_USER_MODEL = 'otp_app.CustomUser'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'travelapptest799@gmail.com'
-EMAIL_HOST_PASSWORD = 'ggtq tjpd eaie royn'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False 
