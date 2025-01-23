293d94412eae81150fec829924ebf211643256a8
rokkunbruv <themorghiknight579@gmail.com>
2025-01-23 21:00:17 +0800

    updated settings.py

--- otp_validation/settings.py
+++ otp_validation/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = 'django-insecure-n9z7m@y8)f31h7=qgw@qgjj-qp&ndr%$l%t0axl^9s*_m5qob%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.16.18.213']
+ALLOWED_HOSTS = [
+    '172.16.18.213',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -136,6 +139,7 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False 
+
 
 CORS_ALLOWED_ORIGINS = [
     '172.16.18.213'
